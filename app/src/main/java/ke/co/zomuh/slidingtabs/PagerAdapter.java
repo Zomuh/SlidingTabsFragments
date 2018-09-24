a4ca1e2efea45d150bb8e66d3f03814ab8c29155
Zomuh <ericcomba96@gmail.com>
2018-09-24 10:54:09 +0300

    Initial commit

---
+++ app/src/main/java/ke/co/zomuh/slidingtabs/PagerAdapter.java
@@ -0,0 +1,43 @@
+package ke.co.zomuh.slidingtabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Zomuh on 23-Sep-18.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter{
+
+    int NoOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumberOfTabs){
+        super(fm);
+        this.NoOfTabs = NumberOfTabs;
+    }
+    @Override
+    public Fragment getItem(int position) {
+
+        switch(position){
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+            case 2:
+                Tab3 tab3 = new Tab3();
+                return tab3;
+            default:
+                return null;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return NoOfTabs;
+    }
+}
